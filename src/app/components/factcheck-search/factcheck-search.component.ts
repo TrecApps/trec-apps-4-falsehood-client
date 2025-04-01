4ca7197aac1debd62343fa400c8d942a0e2d4f91
John Jacko <jljacko@outlook.com>
2025-04-01 09:57:09 -0600

    Completed Draft of Factcheck search

--- src/app/components/factcheck-search/factcheck-search.component.ts
+++ src/app/components/factcheck-search/factcheck-search.component.ts
@@ -3,9 +3,11 @@ import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '@tc/tc-ngx-general';
 import { StylesService } from '../../services/styles.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
 import { Factcheck } from '../../model/Factcheck';
+import { FactcheckService } from '../../services/factcheck.service';
+import ResponseObj from '../../model/ResponseObj';
 
 @Component({
   selector: 'app-factcheck-search',
@@ -24,11 +26,14 @@ export class FactcheckSearchComponent implements OnDestroy{
 
   query: string = "";
 
+  pageSize: number = 20;
+  currentPage: number = 0;
+
 
   factcheckContainer: Factcheck[][] = [];
-  curentFactcheckList: Factcheck[] = [];
+  currentFactcheckList: Factcheck[] = [];
 
-  constructor(ss: StylesService, authService: AuthService, private router: Router){
+  constructor(ss: StylesService, authService: AuthService, private router: Router, private factcheckService: FactcheckService){
     this.styleService = ss;
     this.authService = authService;
 
@@ -49,8 +54,68 @@ export class FactcheckSearchComponent implements OnDestroy{
     this.routerSubscription.unsubscribe();
   }
 
-
-  performSearch(){
-
+  onSelect(factcheck:Factcheck){
+    this.factcheckService.getFactcheck(factcheck.id).subscribe({
+      next: (value: ResponseObj) => {
+        // ToDo: Navigate to Factcheck
+      }
+    })
   }
+
+
+  performSearch(){  
+    this.currentPage = 0;
+    this.factcheckContainer = [];
+    this.currentFactcheckList = [];
+    this.doSearch();
+  }
+
+  isSearching: boolean = false;
+  doSearch(){
+    if(this.isSearching) return;
+    let results: Observable<Factcheck[]>;
+    if(this.mode == 'query'){
+      results = this.factcheckService.searchFactcheckByQuery(this.query,this.currentPage, this.pageSize);
+    } else {
+      results = this.factcheckService.searchFactCheckByMode(this.mode, this.currentPage, this.pageSize);
+    }
+
+    this.isSearching = true;
+
+    results.subscribe({
+      next: (value: Factcheck[]) => {
+        this.factcheckContainer.push(value);
+        this.currentFactcheckList = value;
+        this.isSearching = false;
+      },
+      error: () => {
+        this.isSearching = false;
+      }
+    });
+  }
+
+  onSwitchPage(next: boolean){
+    if(this.isSearching) return;
+    if(next){
+      this.currentPage++;
+      if(this.currentPage == this.factcheckContainer.length){
+        // We need to do another search
+        this.doSearch();
+      } else {
+        this.currentFactcheckList = this.factcheckContainer[this.currentPage];
+      }
+    } else {
+      if(this.currentPage > 0) {
+        this.currentFactcheckList = this.factcheckContainer[--this.currentPage];
+      }
+    }
+  }
+
+  hasMore(): boolean{
+    return (this.factcheckContainer[this.factcheckContainer.length - 1].length == this.pageSize)
+  }
+
+
+
+
 }
