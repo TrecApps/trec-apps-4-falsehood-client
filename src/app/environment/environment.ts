52736aac1624a5c596fa620b11f624eee826c9a4
John Jacko <jljacko@outlook.com>
2025-07-07 07:34:54 -0600

    Tweaks to prepare for Azure deployment

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -2,11 +2,15 @@
 
 export const environment = {
 
-    FALSEHOOD_URL: 'http://localhost:4200/Falsehood-api',
-    RESOURCE_URL: 'http://localhost:4200/Brands-api/',
-    USER_SERVICE_URL: 'http://localhost:4200/User-api/',
-    IMAGE_SERVICE_URL: "http://localhost:4200/Images-api/",
+    // FALSEHOOD_URL: 'http://localhost:4200/Falsehood-api',
+    // RESOURCE_URL: 'http://localhost:4200/Brands-api/',
+    // USER_SERVICE_URL: 'http://localhost:4200/User-api/',
+    // IMAGE_SERVICE_URL: "http://localhost:4200/Images-api/",
 
+    FALSEHOOD_URL: 'https://test.trecapps.com/Falsehood-api',
+    RESOURCE_URL: 'https://test.trecapps.com/Brands-api/',
+    USER_SERVICE_URL: 'https://test.trecapps.com/User-api/',
+    IMAGE_SERVICE_URL: "https://test.trecapps.com/Images-api/",
 
 
 
