9b359fc35325ba7f6ba93717eb75bc40de8451b9
John Jacko <jljacko@outlook.com>
2025-09-10 12:42:49 -0600

    Enabled Dark background in dark mode

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { AuthService } from '@tc/tc-ngx-general';
+import { AuthService, ElementContainerDirective, ElementItemDirective } from '@tc/tc-ngx-general';
 import { environment } from '../../environment/environment';
 import { TopBarComponent } from '../top-bar/top-bar.component';
 
@@ -12,7 +12,7 @@ interface GuidelineEntry{
 
 @Component({
   selector: 'app-welcome',
-  imports: [TopBarComponent],
+  imports: [TopBarComponent, ElementContainerDirective, ElementItemDirective],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
